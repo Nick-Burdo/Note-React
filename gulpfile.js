706ec2ce1efe4e5f31b88ff395f96a418d1837ab
Николай Бурдо <n.burdo@iterios.com>
2016-05-12 17:27:37 +0300

    add empty gulp

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+/**
+ * @copyright Iterios
+ * @author Nick Burdo <n.burdo@iterios.com>
+ * @version 1.0.0  12.05.2016
+ * @since 1.0.0
+ */
+
+var gulp  = require('gulp');
+
+gulp.task('default', function() {
+
+});
